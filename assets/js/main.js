85178cc0e665835fbce670f0d01fdb0ca93caf1a
Dragon336699 <anhruoia1a1@gmail.com>
2023-02-22 20:35:25 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 // Form đăng ký
 var signUp = document.querySelector('.header__navbar-item--sign-up')
-var signUp_form = document.querySelector('.auth-form')
+var signIn = document.querySelector('.header__navbar-item--sign-in');
+var auth_form = document.querySelector('.auth-form')
 var modal = document.querySelector('.modal')
+var register_form = document.querySelector('.register__form')
+var login_form = document.querySelector('.login__form')
 signUp.onclick = function () { 
     modal.style.display = 'flex';
-    signUp_form.style.display = 'block';
+    register_form.style.display = 'block';
+    login_form.style.display = 'none';
+}
+var signUp_back = document.querySelectorAll('.btn--auther-back')
+
+signIn.onclick = function () { 
+    modal.style.display = 'flex';
+    register_form.style.display = 'none'
+    login_form.style.display = 'block';
+}
+
+for (var btn_back in signUp_back){
+    signUp_back.onclick = function(){
+        signUp_back[btn_back].style.display = 'none';
+    }
 }
