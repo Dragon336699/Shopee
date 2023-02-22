b7a0f5563240e40e88e1170efe421fc27f67858c
Dragon336699 <anhruoia1a1@gmail.com>
2023-02-23 00:16:23 +0700

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,37 @@
 // Form đăng ký
 var signUp = document.querySelector('.header__navbar-item--sign-up')
 var signIn = document.querySelector('.header__navbar-item--sign-in');
-var auth_form = document.querySelector('.auth-form')
-var modal = document.querySelector('.modal')
-var register_form = document.querySelector('.register__form')
-var login_form = document.querySelector('.login__form')
+var auth_form = document.querySelector('.auth-form');
+var modal = document.querySelector('.modal');
+var register_form = document.querySelector('.register__form');
+var login_form = document.querySelector('.login__form');
+var modal__inner = document.querySelector('.modal__inner');
 signUp.onclick = function () { 
     modal.style.display = 'flex';
     register_form.style.display = 'block';
     login_form.style.display = 'none';
 }
 var signUp_back = document.querySelectorAll('.btn--auther-back')
-
 signIn.onclick = function () { 
     modal.style.display = 'flex';
     register_form.style.display = 'none'
     login_form.style.display = 'block';
 }
-
-for (var btn_back in signUp_back){
-    signUp_back.onclick = function(){
-        signUp_back[btn_back].style.display = 'none';
+for (var btn_back of signUp_back){
+    btn_back.onclick = function () { 
+        modal.style.display = 'none'; 
     }
 }
+
+// Convert between sign in and sign up
+var convertToSignIn = document.querySelector('.auth-form__convert-btn-register');
+var convertToSignUp = document.querySelector('.auth-form__convert-btn-login');
+convertToSignIn.onclick = function () {  
+    register_form.style.display = 'none'
+    login_form.style.display = 'block';
+}
+convertToSignUp.onclick = function () {  
+    register_form.style.display = 'block';
+    login_form.style.display = 'none';
+}
+
