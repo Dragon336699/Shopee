ad8d54cd3850bead7ea9dfbbafd609d197d57e26
Dragon336699 <anhruoia1a1@gmail.com>
2023-02-23 22:43:59 +0700

    pull

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,32 @@ signIn.onclick = function () {
     login_form.style.display = 'block';
 }
 
-for (var btn_back in signUp_back){
-    signUp_back.onclick = function(){
-        signUp_back[btn_back].style.display = 'none';
+for (var btn_back of signUp_back){
+    btn_back.onclick = function(){
+        modal.style.display = 'none';
+    }
+}
+
+// Convert between sign in and sign up
+var convertToSignIn = document.querySelector('.auth-form__convert-btn-register');
+var convertToSignUp = document.querySelector('.auth-form__convert-btn-login');
+convertToSignIn.onclick = function () {  
+    register_form.style.display = 'none'
+    login_form.style.display = 'block';
+}
+convertToSignUp.onclick = function () {  
+    register_form.style.display = 'block';
+    login_form.style.display = 'none';
+}
+
+var filterClicked;
+var buttonFilters = document.querySelectorAll('.btn-home-filter');
+for (var buttonFilter of buttonFilters){
+    buttonFilter.onclick = function (event){
+        filterClicked = event.target;
+        for (buttonFilter of buttonFilters){
+            buttonFilter.classList.remove ('btn--active')
+        }
+        filterClicked.classList.add('btn--active')
     }
 }
